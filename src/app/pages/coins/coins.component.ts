1df5eca2fadea8987e31b68bf915160a77e61408
Malva37 <irynapysanko@gmail.com>
2020-02-22 21:59:06 +0200

    func. maxPrice

--- src/app/pages/coins/coins.component.ts
+++ src/app/pages/coins/coins.component.ts
@@ -10,30 +10,49 @@ import { Options } from 'ng5-slider';
 })
 export class CoinsComponent implements OnInit {
 
-  constructor(private prService:ProductsService) { }
-coins:Array<ICoin>=[];
-buttonsShow:boolean;
-minValue: number = 10;
-maxValue: number = 2000;
-options: Options = {
-  floor: 0,
-  ceil: 2000,
-  // step: 10,
-  showTicks: true
-};
+  constructor(private prService: ProductsService) { }
+  coins: Array<ICoin> = [];
+  productPrices: Array<number> = [];
+  buttonsShow: boolean;
+  minValue: number = 10;
+  maxValue: number;
+  options: Options = {
+    floor: 0,
+    ceil: 2000,
+    showTicks: true
+  };
+
 
   ngOnInit() {
-    this.getCoins()
+    this.getCoins();
   }
 
+  getMaxPrice(arr:Array<number>): number {
+    arr = this.productPrices
+    let max = arr[0];
+    for (let i = 0; i <= arr.length; i++) {
+      if (i > max) {
+        i = max;
+      }
+      return this.maxValue = max;
+    }
+  }
 
 
   private getCoins(): void {
     this.prService.getJSONCoins().subscribe(
       data => {
         this.coins = data;
-      }
-    )
+        for (const coin in this.coins) {
+          const object = this.coins[coin];
+          this.productPrices.push(object.price);
+        }
+      })
+    this.getMaxPrice();
   }
+
+
+
+
 
 }
