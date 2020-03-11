00e0c1ecc9e403c9d566ac604daa4de47f72da6a
Malva37 <irynapysanko@gmail.com>
2020-03-11 17:52:46 +0200

    done admin-orders part

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageReference, AngularFireUploadTask, AngularFireStorage } from '@angular/fire/storage';
+import { Order } from 'src/app/shared/classes/orders.model';
+import { OrderService } from 'src/app/shared/services/order.service';
 
 @Component({
   selector: 'app-admin-orders',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-orders.component.scss']
 })
 export class AdminOrdersComponent implements OnInit {
+  list: Order[];
+  price: number;
+  image: string;
+  name: string;
 
-  constructor() { }
+
+  constructor(    private service: OrderService,
+    private firestore: AngularFirestore,
+    private afStorage: AngularFireStorage) { }
 
   ngOnInit() {
+
+    this.service.getOrders().subscribe(actionArray => {
+      this.list = actionArray.map(item => {
+        return {
+          id: item.payload.doc.id, ...item.payload.doc.data()
+        } as Order;
+      });
+    });
+    // this.resetForm();
   }
+
+  onDelete(order: Order) {
+    if (confirm('Are you sure to delete this order?')) {
+      this.firestore.doc('orders/' + order.id).delete();
+      // this.afStorage.storage.refFromURL(medal.image).delete();
+    }
+  }
+
+
 
 }
