55cc8f5d4d9b14d3929a16cde6c8e2c8005469dc
Iryna Pysanko <58560038+Malva37@users.noreply.github.com>
2020-05-04 12:06:22 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: `process.env.API_KEY`,
-    authDomain: `process.env.AUTH_DOMAIN`,
-    databaseURL: `process.env.DATABASE_URL`,
-    projectId: `process.env.PROJECT_ID`,
-    storageBucket: `process.env.STORAGE_BUCKET`,
-    messagingSenderId: `process.env.MESSAGING_SENDER_ID`,
-    appId: `process.env.APP_ID`
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   },
-  apiUrl: `process.env.API_URL`,
+  apiUrl: process.env.API_URL
 };
